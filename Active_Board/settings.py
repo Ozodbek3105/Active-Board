65402f205e26a82b3e6bfd3ad2e1a8b12b965623
ozodbek <xudoyberdiyevozodbek04@gmail.com>
2025-02-23 21:17:59 +0500

    statik fayllar togirlandi

--- Active_Board/settings.py
+++ Active_Board/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'Active_Board.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,"templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Active_Board/static",  # Loyiha ichidagi statik fayllar papkasi
+]
+STATIC_ROOT = BASE_DIR / "static"  # Yig‘ilgan fayllar uchun alohida papka
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -6,6 +6,8 @@ class Position(models.Model):
 
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
     
 # Create your models here.
 class Users(models.Model):
